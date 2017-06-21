8663f30c5a7215a22aad0360c2baf58490f103cb
eleanorstrib <hello@eleanorstrib.com>
2017-06-20 22:10:18 -0700

    added heroku url to allowed_hosts list in settings.py

--- sampledeploy/sampledeploy/settings.py
+++ sampledeploy/sampledeploy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', 'stark-escarpment-63219.herokuapp.com']
 
 
 # Application definition
